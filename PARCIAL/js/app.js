19a1246638e055e42a05a9b43fd52d8cea3eb271
Aime2630 <104107482+Aime2630@users.noreply.github.com>
2024-11-08 19:18:15 -0400

    Subida

---
+++ PARCIAL/js/app.js
@@ -0,0 +1,6 @@
+AOS.init();
+
+// Función para redirigir a la página principal o de donación
+function donateNow() {
+    window.location.href = "https://www.tusitioprincipal.com"; // Coloca aquí el enlace a tu página de donación o principal
+}
